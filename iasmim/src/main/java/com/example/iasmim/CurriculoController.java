723d0ebbdb006c12fd8c03e9418b320a1477973b
IasmimHolanda <106707727+IasmimHolanda@users.noreply.github.com>
2023-12-04 22:06:46 +0000

    update

--- iasmim/src/main/java/com/example/iasmim/CurriculoController.java
+++ iasmim/src/main/java/com/example/iasmim/CurriculoController.java
@@ -6,36 +6,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.String;
 import java.util.List;
 
 @RestController
 @RequestMapping("/iasmim/curriculos")
 public class CurriculoController {
+    
     @Autowired
     private CurriculoService curriculoService;
 
     @GetMapping
-    public List<Curriculo> listarCurriculos() {
-        return curriculoService.listarCurriculos();
+    public ResponseEntity<List<Curriculo>> listarCurriculos() {
+        List<Curriculo> curriculos = curriculoService.listarCurriculos();
+        return ResponseEntity.ok(curriculos);
     }
 
     @GetMapping("/{id}")
-    public Curriculo buscarCurriculo(@PathVariable Long id) {
-        return curriculoService.buscarCurriculo(id);
+    public ResponseEntity<Curriculo> buscarCurriculo(@PathVariable Long id) {
+        Curriculo curriculo = curriculoService.buscarCurriculo(id);
+        return ResponseEntity.ok(curriculo);
     }
 
     @PostMapping
-    public Curriculo cadastrarCurriculo(@RequestBody Curriculo curriculo) {
-        return curriculoService.cadastrarCurriculo(curriculo);
+    public ResponseEntity<Curriculo> cadastrarCurriculo(@RequestBody Curriculo curriculo) {
+        Curriculo novoCurriculo = curriculoService.cadastrarCurriculo(curriculo);
+        return ResponseEntity.ok(novoCurriculo);
     }
 
     @PutMapping("/{id}")
-    public Curriculo atualizarCurriculo(@PathVariable Long id, @RequestBody Curriculo curriculo) {
-        return curriculoService.atualizarCurriculo(id, curriculo);
+    public ResponseEntity<Curriculo> atualizarCurriculo(@PathVariable Long id, @RequestBody Curriculo curriculo) {
+        Curriculo curriculoAtualizado = curriculoService.atualizarCurriculo(id, curriculo);
+        return ResponseEntity.ok(curriculoAtualizado);
     }
 
     @DeleteMapping("/{id}")
-    public void deletarCurriculo(@PathVariable Long id) {
+    public ResponseEntity<Void> deletarCurriculo(@PathVariable Long id) {
         curriculoService.deletarCurriculo(id);
+        return ResponseEntity.noContent().build();
     }
 }
