866d0ac65729c2578790e68e21a71374ccec24df
IasmimHolanda <106707727+IasmimHolanda@users.noreply.github.com>
2023-12-06 12:46:55 +0000

    test

--- main/java/com/example/iasmim/CurriculoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.iasmim;
-
-import com.example.iasmim.Curriculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurriculoRepository extends JpaRepository<Curriculo, Long> {
-}
